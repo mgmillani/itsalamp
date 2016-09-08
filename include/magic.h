58a74b7487ead88994c40635c836a5d4b2e35b42
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2016-09-08 14:57:18 +0200

    Helper macros.

---
+++ include/magic.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2016 Marcelo Garlet Millani
+
+This file is part of itsalamp.
+
+itsalamp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+itsalamp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with itsalamp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+#define MIN(x,y) ((x) > (y) ? (y) : (x))
