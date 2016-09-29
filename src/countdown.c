95ab760e93fdb821c847c80887c7033ecc013670
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2016-09-29 18:49:48 +0200

    Reads commands from stdin.
    It is possible to either reset the timer with 'reset' or set a new time.

--- src/countdown.c
+++ src/countdown.c
@@ -4,18 +4,20 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include <math.h>
 
 #include "parse.h"
 #include "color.h"
 
+#include <sys/select.h>
 #include <unistd.h>
 
 #include "debug.h"
 //#include "magic.h"
 
-// flags for what must chnage
+// flags for what must change
 #define CH_NOTHING 0
 #define CH_COLOR   1
 #define CH_TEXT    2
@@ -23,6 +25,63 @@
 
 #define HOUR 3600
 #define MINUTE 60
+
+typedef struct s_timer
+{
+	int seconds;             // total number of seconds
+	double elapsed;          // time elapsed since the beginning
+	double nextIconT;	       // when do we need to change the icon
+	double nextTextT;        // when do we need to change the tooltip text
+	double color0[3];        // initial color
+	double color[3];         // current color
+	double color1[3];        // final color
+	double colorStep[3];     // color change per second
+	unsigned char color8[3]; // color in 8-bit per channel
+	double iStep;
+	double brightness0;      // original brightness
+	double brightness;       // current brightness
+	double brightStep;       // brightness change per second
+	int iconI;               // index of the icon in argv
+}t_timer;
+
+void setupTimer(t_timer *t, int numicons, time_t t0)
+{
+	// how much the color should be incremented per second
+	int c;
+	for(c = 0; c<3 ; c++)
+	{
+		t->colorStep[c] = (t->color1[c] - t->color0[c]) / (double) t->seconds;
+		t->color8[c] = t->color0[c]*255;
+		t->color[c] = t->color0[c];
+	}
+	TRACE("colorStep: %lf %lf %lf", t->colorStep[0], t->colorStep[1], t->colorStep[2]);
+	// brightness according to "HSP"
+	double R = t->color0[0];
+	double G = t->color0[1];
+	double B = t->color0[2];
+	t->color[0] = R;
+	t->color[1] = G;
+	t->color[2] = B;
+	t->brightness0 = sqrt(R*R*Pr + G*G*Pg + B*B*Pb);
+	t->brightness = t->brightness0;
+	R = t->color1[0];
+	G = t->color1[1];
+	B = t->color1[2];
+	double brightness1 = sqrt(R*R*Pr + G*G*Pg + B*B*Pb);
+	t->brightStep = (brightness1 - t->brightness0) / t->seconds;
+
+	TRACE("b0 = %lf\tb1 = %lf", t->brightness0, brightness1);
+
+	// how long should we wait before changing the icon
+	t->iStep = t->seconds / (double) (numicons);
+
+	time_t t1 = time(NULL);
+	t->elapsed = difftime(t1,t0);
+	t->nextIconT = t->iStep;
+	// when do we need to update the minutes
+	t->nextTextT = t->seconds % 60;
+	t->iconI = 0;
+}
 
 /**
  * Prints the time specified by seconds.
@@ -50,7 +109,7 @@ void printTime(int seconds)
 void printColor(unsigned char color[])
 {
 	static char alpha[] = "0123456789ABCDEF";
-	printf("%c%c%c%c%c%c"
+	printf("#%c%c%c%c%c%c"
 		, alpha[(color[0] >> 4) & 0xf]
 		, alpha[(color[0])      & 0xf]
 		, alpha[(color[1] >> 4) & 0xf]
@@ -126,7 +185,7 @@ int parseRGB(char *str, double *RGB)
 		}
 		else
 		{
-			color |= FROM_HEXA(toupper(str[i]));
+			color |= FROM_HEXA(toupper(str[i])); // program flow guarantees that the value is initialized by now
 			RGB[cl] = (double) color / 255.0;
 			most = 1;
 			cl++;
@@ -140,6 +199,72 @@ int parseRGB(char *str, double *RGB)
 		return 1;
 }
 
+int waitForInput(t_timer *t, int numicons, time_t *t0, char **icon)
+{
+	fd_set rfds;
+	struct timeval tv;
+	int retval;
+	int restart = 0;
+
+	FD_ZERO(&rfds);
+	FD_SET(0, &rfds); // stdin = 0
+
+	// up to one second
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+
+	retval = select(1, &rfds, NULL, NULL, &tv);
+
+	// if there is something to read
+	if(retval > 0)
+	{
+		int textLen = 1024;
+		char text[textLen];
+		fgets(text, textLen, stdin);
+		int i;
+		for(i=0 ; text[i] != '\0' ; i++)
+		{
+			text[i] = tolower(text[i]);
+			if(text[i] == '\n')
+				text[i] = '\0';
+		}
+		TRACE("text: %s", text);
+		// reset timer
+		if(strcmp(text, "reset") == 0)
+		{
+			time(t0);
+			setupTimer(t, numicons, *t0);
+			printColor(t->color8);
+			printf(":'%s' ", icon[t->iconI]);
+			printTime(t->seconds - t->elapsed);
+			fflush(stdout);
+			restart = 1;
+		}
+		else
+		{
+			// check if the user changed the time
+			int seconds = parseTime(text);
+			if(seconds != -1)
+			{
+				t->seconds = seconds;
+				time(t0);
+				setupTimer(t, numicons, *t0);
+				printColor(t->color8);
+				printf(":'%s' ", icon[t->iconI]);
+				printTime(t->seconds - t->elapsed);
+				fflush(stdout);
+				restart = 1;
+			}
+		}
+	}
+	else if(retval == -1)
+	{
+		perror("waitForInput");
+	}
+
+	return restart;
+}
+
 int main(int argc, char *argv[])
 {
 	time_t t0 = time(NULL);
@@ -149,128 +274,101 @@ int main(int argc, char *argv[])
 		ERR("usage: %s <[HOURSh][MINUTSm][SECONDSs]> <INITIAL COLOR> <FINAL COLOR> <ICONS...>", argv[0]);
 		exit(1);
 	}
+	t_timer t;
 
-	int seconds = parseTime(argv[1]);
-	if(seconds < 0)
+	t.seconds = parseTime(argv[1]);
+	if(t.seconds < 0)
 	{
 		ERR("Wrong time format. Use something like '15m10s' (provided: %s).", argv[1]);
 		exit(1);
 	}
 
-	double color0[3];
-	if(parseRGB(argv[2], color0) != 0)
+	if(parseRGB(argv[2], t.color0) != 0)
 	{
 		ERR("Wrong color format. Use something like 'FF00CC'. (provided: %s)", argv[2]);
 		exit(1);
 	}
-	double color1[3];
-	if(parseRGB(argv[3], color1) != 0)
+
+	if(parseRGB(argv[3], t.color1) != 0)
 	{
 		ERR("Wrong color format. Use something like 'FF00CC'. (provided: %s)", argv[3]);
 		exit(1);
 	}
 
-	TRACE("color0: %.2lf %.2lf %.2lf", color0[0], color0[1], color0[2]);
-	TRACE("color1: %.2lf %.2lf %.2lf", color1[0], color1[1], color1[2]);
+	TRACE("color0: %.2lf %.2lf %.2lf", t.color0[0], t.color0[1], t.color0[2]);
+	TRACE("color1: %.2lf %.2lf %.2lf", t.color1[0], t.color1[1], t.color1[2]);
 
 	int numicons = argc - 4;
 
-	// how much the color should be incremented per second
-	double colorStep[3];
-	unsigned char color8[3];
-	int c;
-	for(c = 0; c<3 ; c++)
-	{
-		colorStep[c] = (color1[c] - color0[c]) / (double) seconds;
-		color8[c] = color0[c]*255;
-	}
+	setupTimer(&t, numicons, t0);
 
-	TRACE("colorStep: %lf %lf %lf", colorStep[0], colorStep[1], colorStep[2]);
-
-	// brightness according to "HSP"
-	double R = color0[0];
-	double G = color0[1];
-	double B = color0[2];
-	double brightness0 = sqrt(R*R*Pr + G*G*Pg + B*B*Pb);
-	R = color1[0];
-	G = color1[1];
-	B = color1[2];
-	double brightness1 = sqrt(R*R*Pr + G*G*Pg + B*B*Pb);
-	double brightStep = (brightness1 - brightness0) / seconds;
-
-	TRACE("b0 = %lf\tb1 = %lf", brightness0, brightness1);
-
-	// how long should we wait before changing the icon
-	double iStep;
-	iStep = seconds / (double) (numicons);
-
-	time_t t1 = time(NULL);
-	double elapsed = difftime(t1,t0);
-	double nextIconT = iStep;
-	// when do we need to update the minutes
-	double nextTextT = seconds % 60;
-	int iconI = 4;
+	char **icon = argv+4;
 
 	// initial status
-	printf("%s:'%s' ", argv[2], argv[iconI]);
+	printf("#%s:'%s' ", argv[2], icon[t.iconI]);
+	printTime(t.seconds);
 	fflush(stdout);
-	printTime(seconds);
 
-	while(elapsed < seconds)
+	time_t t1;
+
+	while(t.elapsed < t.seconds)
 	{
+		// sleeps for one second, waiting for input on stdin
+		while(waitForInput(&t, numicons, &t0, icon))
+			;
+		t1 = time(NULL);
+		t.elapsed = difftime(t1,t0);
+
 		// what do we need to change now
 		char change = CH_NOTHING;
 
 		// updates color
+		int c;
 		for(c = 0; c<3 ; c++)
 			// adds the step, clamping to [0, 1.0]
-			color0[c] = MIN(1.0, MAX(colorStep[c] + color0[c], 0));
+			t.color[c] = MIN(1.0, MAX(t.colorStep[c] + t.color[c], 0));
 
-		brightness0 += brightStep;
+		t.brightness += t.brightStep;
 		double HSP[3];
-		RGBtoHSP(color0, HSP);
-		HSP[2] = brightness0;
-		HSPtoRGB(HSP, color0);
+		RGBtoHSP(t.color, HSP);
+		HSP[2] = t.brightness;
+		HSPtoRGB(HSP, t.color);
 		// check if there was significant change in color
 		for(c = 0; c<3 ; c++)
 		{
-			if(color0[c]*255 != color8[c])
+			if(t.color[c]*255 != t.color8[c])
 			{
 				change |= CH_COLOR;
-				color8[c] = color0[c] * 255;
+				t.color8[c] = t.color[c] * 255;
 			}
 		}
 
 		// time to change icon
-		if(elapsed > nextIconT)
+		if(t.elapsed > t.nextIconT)
 		{
 			change |= CH_ICON;
-			iconI++;
-			nextIconT += iStep;
+			t.iconI++;
+			t.nextIconT += t.iStep;
 		}
 		// once there are less than 5 minutes left, start counting the seconds
 		// minutes are always updated
-		if(seconds - elapsed < 5*60 || elapsed > nextTextT)
+		if(t.seconds - t.elapsed < 5*60 || t.elapsed > t.nextTextT)
 		{
 			change |= CH_TEXT;
-			nextTextT += 60;
+			t.nextTextT += 60;
 		}
 
 		// we need to update something
 		if(change != CH_NOTHING)
 		{
 			TRACE("update");
-			printColor(color8);
+			printColor(t.color8);
 			// if we change the icon, we update everything
 			if(change & CH_ICON)
-				printf(":'%s'", argv[iconI]);
-			printTime(seconds - elapsed);
+				printf(":'%s'", icon[t.iconI]);
+			printTime(t.seconds - t.elapsed);
 			fflush(stdout);
 		}
-		// sleeps for one second
-		usleep(1000000);
-		t1 = time(NULL);
-		elapsed = difftime(t1,t0);
 	}
 
 	return 0;
